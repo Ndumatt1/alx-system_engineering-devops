fe3416d5a90b7c807d1997eaef8e4d812fa18090
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-05-18 23:14:12 +0100

    fix error

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,7 +3,7 @@ exec { 'increase-ulimit':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/',
 }
-exec { 'nginx-restart':
+-> exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
 }
