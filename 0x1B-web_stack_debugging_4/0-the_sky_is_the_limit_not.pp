33de6d74780d67c1b6c786b0dd0131898d721345
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-05-18 23:02:06 +0100

    upload 1-user_limit and fix 0-the-sky-is_the_limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # update the ulimit of the nginx process
 exec { 'increase-ulimit':
-  command => 'sed -i "s/15/20000/" /etc/default/nginx',
-  path    => ['/usr/local/bin/', '/bin/'],
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/',
 }
-exec { 'restart':
+exec { 'nginx-restart':
   command => 'nginx restart',
-  path    => ['/etc/init.d/']
+  path    => '/etc/init.d/'
 }
---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# increase system nofile for holberton user
+
+exec { 'increase-hard-file-limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'increase-soft-file-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
